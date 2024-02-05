e66bcb9c3df2304eb475e048449bc4966c12ab78
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 14:57:54 +0100

    Create apply.sh

---
+++ apply.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+#
+source bash/avi_api.sh
+#
+yq -c -r vars.yml | tee vars.json
+jsonFile="vars.json"
+#
+if [[ $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "emea" && \
+      $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "us" && \
+      $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "apj" && ]] ; then
+  echo "   +++ .zone should equal to one of the following: 'emea, us, apj'"
+  exit 255
+fi
