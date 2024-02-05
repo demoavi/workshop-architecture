aa6341df87b05e5fed6b8c7b48e600e4d99c403b
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 16:33:57 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -7,7 +7,7 @@ jsonFile="vars.json"
 #
 if [[ $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "emea" && \
       $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "us" && \
-      $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "apj" && ]] ; then
+      $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "apj" ]] ; then
   echo "   +++ .zone should equal to one of the following: 'emea, us, apj'"
   exit 255
 fi
