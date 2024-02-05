1d16ac30ccd146811be9d788481b00510486847c
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-05 16:52:18 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -11,3 +11,17 @@ if [[ $(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:]) != "emea" && \
   echo "   +++ .zone should equal to one of the following: 'emea, us, apj'"
   exit 255
 fi
+#
+zone=$(jq -c -r '.zone' $jsonFile | tr '[:upper:]' [:lower:])
+avi_auth_file="/home/ubuntu/.avicreds-${zone}.json"
+avi_cookie_file="/home/ubuntu/avi_cookie_${zone}.txt"
+rm -f ${avi_cookie_file}
+#
+avi_username=$(jq -c -r .avi_credentials.username $avi_auth_file)
+avi_password=$(jq -c -r .avi_credentials.password $avi_auth_file)
+avi_controller=$(jq -c -r .avi_credentials.controller $avi_auth_file)
+avi_version=$(jq -c -r .avi_credentials.api_version $avi_auth_file)
+#
+curl_login=$(curl -s -k -X POST -H "Content-Type: application/json" \
+                                -d "{\"username\": \"${avi_username}\", \"password\": \"${avi_password}\"}" \
+                                -c ${avi_cookie_file} https://${avi_controller}/login)
