88c0c173d3797e734cf5536185e96235eb9d7937
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-06 10:50:12 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -131,7 +131,7 @@ if [[ ${tenant_count} == 1 && ${user_count} == 1 && ${create} == "true" ]] ; the
          }
        ]
     }'
-    echo ${json_data}
+    echo ${json_data} | jq -c -r '.'
     alb_api 2 1 "POST" "${avi_cookie_file}" "${csrftoken}" "admin" "${avi_version}" "${json_data}" "${avi_controller}" "api/vsvip"
     ((count++))
   done
