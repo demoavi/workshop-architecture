96aa0c8c15de5ef0534f6ac44ff2d1d67aa8de8a
demoavi <157693200+demoavi@users.noreply.github.com>
2024-02-06 12:02:43 +0100

    Update apply.sh

--- apply.sh
+++ apply.sh
@@ -138,7 +138,7 @@ if [[ ${tenant_count} == 1 && ${user_count} == 1 && ${create} == "true" ]] ; the
     {
       "cloud_ref": "/api/cloud/?name='$(jq -c -r '.cloud.name' ${avi_settings_file})'",
       "name": "'$(jq -c -r '.tenant.basename' ${avi_settings_file})${count}''$(jq -c -r '.pool.basename' ${avi_settings_file})'",
-      "servers": "'$(jq -c -r '.pool.servers' ${avi_settings_file})'"
+      "servers": '$(jq -c -r '.pool.servers' ${avi_settings_file})'
     }'
     echo ${json_data}
     echo ${json_data} | jq -c -r '.'
