55a7b4c2346f058270aa9d3b0cb7a5468e5235e4
demoavi <157693200+demoavi@users.noreply.github.com>
2024-03-05 18:51:00 +0200

    Update apply.sh cleaning

--- apply.sh
+++ apply.sh
@@ -266,7 +266,7 @@ if [[ ${create} == "false" ]] ; then
   #done
   #
   IFS=$'\n'
-  list_object_to_remove='["virtualservice", "pool", "healthmonitor", "vsvip", "networksecuritypolicy", "applicationprofile", "serviceengine", "serviceenginegroup", "analyticsprofile", "wafpolicypsmgroup", "wafpolicy"]'
+  list_object_to_remove='["virtualservice", "pool", "healthmonitor", "vsvip", "networksecuritypolicy", "applicationprofile", "serviceengine", "serviceenginegroup", "analyticsprofile", "wafpolicy", "wafpolicypsmgroup"]'
   for object_to_remove in $(echo $list_object_to_remove | jq -c -r .[])
   do
     if [[ ${object_to_remove} == "serviceenginegroup" && ${se_deletion} == "true" ]] ; then
