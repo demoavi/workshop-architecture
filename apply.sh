b32616427f30ca2d771c4c79e3dc6a37e423a700
demoavi <157693200+demoavi@users.noreply.github.com>
2024-03-06 17:48:00 +0200

    Update apply.sh provider mode

--- apply.sh
+++ apply.sh
@@ -216,8 +216,8 @@ if [[ ${create} == "false" ]] ; then
       item_tenant_uuid=$(echo ${item} | jq -c -r '.tenant_ref' | grep / | cut -d/ -f6-)
       item_tenant_name=$(echo ${tenant_results} | jq -c -r --arg arg "${item_tenant_uuid}" '.[] | select( .uuid == $arg ) | .name')
       if [[ ${object_to_remove} == "serviceengine" ]] ; then
-        if $(echo $item | jq -e '.vs_refs' > /dev/null)
-          echo "++++ se ${item_name} has vs"
+        if $(echo $item | jq -e '.vs_refs' > /dev/null) ; then
+          echo "++++ se ${item_name} is busy with vs"
         else
           se_deletion="true"
           alb_api 3 5 "DELETE" "${avi_cookie_file}" "${csrftoken}" "${item_tenant_name}" "${avi_version}" "" "${avi_controller}" "$(echo ${item_url} | grep / | cut -d/ -f4-)"
